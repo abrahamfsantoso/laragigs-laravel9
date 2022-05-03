798b06c256bd50ae2ae9693bbd04732d9ff6c3a6
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-05-03 20:56:15 +0700

    add registration and logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,39 +13,54 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
-                theme: {
-                    extend: {
-                        colors: {
-                            laravel: "#ef3b2d",
-                        },
-                    },
-                },
-            };
+        theme: {
+          extend: {
+            colors: {
+              laravel: '#ef3b2d',
+            },
+          },
+        },
+      }
     </script>
     <title>LaraGigs | Find Laravel Jobs & Projects</title>
 </head>
 
 <body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
-        <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo" /></a>
+        <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
+            @auth
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <span class="font-bold uppercase">
+                    Welcome {{auth()->user()->name}}
+                </span>
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                    Login</a>
+                <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage
+                    Listings</a>
             </li>
+            <li>
+                <form class="inline" method="POST" action="/logout">
+                    @csrf
+                    <button type="submit">
+                        <i class="fa-solid fa-door-closed"></i> Logout
+                    </button>
+                </form>
+            </li>
+            @else
+            <li>
+                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+            </li>
+            <li>
+                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+            </li>
+            @endauth
         </ul>
     </nav>
 
     <main>
-        {{-- View Output --}}
-        {{-- @yield('content') --}}
-        {{ $slot }}
+        {{$slot}}
     </main>
-
-
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,3 +63,13 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 //     ]);
 // });
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
