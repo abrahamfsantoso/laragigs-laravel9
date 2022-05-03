9e1c566110d88871d8ff85e31c94f489877ca989
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-05-03 22:10:01 +0700

    relationship and ownership to listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
-        // $user = User::factory()->create([
-        //     'name' => 'John Doe',
-        //     'email' => 'john@gmail.com'
-        // ]);
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
 
-        Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
         // Listing::create([
         //     'title' => 'Laravel Senior Developer',
