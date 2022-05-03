3bb21437da3ae5e883c45b891daab9496cdf397a
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-05-03 15:58:49 +0700

    add blade and tags components

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,13 @@ Route::get('/', function () {
 });
 
 // Single Listing
-Route::get('/listings/{id}', function ($id) {
+// Route::get('/listings/{id}', function ($id) {
+//     return view('listing', [
+//         'listing' => Listing::find($id)
+//     ]);
+// });
+Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });
