be9960654f600a47077e629178129c74a23e0e96
Abraham F. Santoso <abrahamfsantoso@gmail.com>
2022-05-03 13:31:10 +0700

    create Listing model and factory

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dignissimos repellendus possimus quidem iure porro illum consequuntur nihil magnam inventore dolore ullam officia optio, qui cupiditate? Dolorem architecto nam, corporis quia ex perferendis eos corrupti. Optio nemo eos ullam ex, tempore quasi laboriosam non, quis ea perferendis commodi? Sapiente, illo!"
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dignissimos repellendus possimus quidem iure porro illum consequuntur nihil magnam inventore dolore ullam officia optio, qui cupiditate? Dolorem architecto nam, corporis quia ex perferendis eos corrupti. Optio nemo eos ullam ex, tempore quasi laboriosam non, quis ea perferendis commodi? Sapiente, illo!"
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
